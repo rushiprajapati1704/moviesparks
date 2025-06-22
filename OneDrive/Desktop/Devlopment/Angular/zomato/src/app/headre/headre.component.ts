c9a6ea524b82d828175af4f054b45abe8332ddf8
rushiprajapati1704 <114585583+rushiprajapati1704@users.noreply.github.com>
2025-06-22 17:33:10 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/Devlopment/Angular/zomato/src/app/headre/headre.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-headre',
-  templateUrl: './headre.component.html',
-  styleUrl: './headre.component.css',
-})
-export class HeadreComponent {
-
-}
